a9f8b0641be5e13eb35c7ba5e0d472c953521aa5
yuanjie <yuanjie1224@gmail.com>
2023-10-09 16:44:20 +0800

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'dart:async';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:soundpool/soundpool.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:video_player/video_player.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -22,6 +24,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MusicManger {
+  static final musicManger = MusicManger(url: "assets/audio/bgm.mp3");
+  String url;
+  final AudioPlayer player = AudioPlayer();
+
+  MusicManger({required this.url, this.isPlay = true}) {
+    setUrl(url);
+  }
+
+  bool isPlay;
+
+  Future<void> setUrl(String url) async {
+    await player.setAsset(url);
+    player.setLoopMode(LoopMode.all);
+  }
+}
+
 class WeddingInvitePage extends StatefulWidget {
   WeddingInvitePage({super.key});
 
@@ -30,6 +49,7 @@ class WeddingInvitePage extends StatefulWidget {
 }
 
 class _WeddingInvitePageState extends State<WeddingInvitePage> {
+  var bgmManger = MusicManger.musicManger;
   late VideoPlayerController _controller;
   ScrollPhysics scrollPhysics = const ScrollPhysics();
   bool isShow = true;
@@ -37,33 +57,39 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
   bool isShow1 = true;
   String name = "";
   String number = "";
-
-
-  AudioPlayer player = AudioPlayer();
-
+  bool showLoding = true;
+  String loadingMsg = "请柬正在加载中...";
 
   @override
   void initState() {
     _controller = VideoPlayerController.asset("assets/videos/inviteVideos.mp4")
       ..initialize().then((value) => () {
-            setState(() async {
+            setState(() {
               _controller.play();
-              await player.play(AssetSource('assets/audio/bgm.mp3'));
             });
           });
     _controller.addListener(() {
       VideoPlayerValue value = _controller.value;
       if (value.isCompleted) {
         setState(() {
-          print("object");
           videoPage = false;
         });
       }
     });
     super.initState();
+    int count = 0;
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      count++;
+      if (count == 6) {
+        setState(() {
+          showLoding = false;
+          timer.cancel();
+        });
+      }
+    });
   }
 
-  TextStyle _textStyle = const TextStyle(
+  final TextStyle _textStyle = const TextStyle(
       color: Colors.white,
       fontSize: 14,
       fontWeight: FontWeight.w700,
@@ -75,8 +101,7 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
         body: Stack(children: [
       Image.asset(
         "assets/images/AM2I7743.jpg",
-        fit: BoxFit.fitHeight,
-        height: double.infinity,
+        fit: BoxFit.cover,
       ),
       PageView(scrollDirection: Axis.vertical, children: [
         HotelPageOne(textStyle: _textStyle),
@@ -123,13 +148,13 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
                                       name = text;
                                     });
                                   },
-                                  style: TextStyle(
+                                  style: const TextStyle(
                                     color: Colors.white,
                                     fontFamily: "NotoSerifSC",
                                   ),
                                   keyboardType: TextInputType.text,
                                   cursorColor: Colors.white,
-                                  decoration: InputDecoration(
+                                  decoration: const InputDecoration(
                                     enabledBorder: OutlineInputBorder(
                                       borderSide:
                                           BorderSide(color: Colors.white),
@@ -162,12 +187,12 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
                                     });
                                   },
                                   keyboardType: TextInputType.number,
-                                  style: TextStyle(
+                                  style: const TextStyle(
                                     color: Colors.white,
                                     fontFamily: "NotoSerifSC",
                                   ),
                                   cursorColor: Colors.white,
-                                  decoration: InputDecoration(
+                                  decoration: const InputDecoration(
                                     enabledBorder: OutlineInputBorder(
                                       borderSide:
                                           BorderSide(color: Colors.white),
@@ -218,7 +243,7 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
                                     barrierDismissible: false,
                                     builder: (BuildContext context) {
                                       return AlertDialog(
-                                        content: SingleChildScrollView(
+                                        content: const SingleChildScrollView(
                                             child: Text(
                                           "提交完成",
                                           style: TextStyle(
@@ -227,7 +252,7 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
                                         )),
                                         actions: <Widget>[
                                           TextButton(
-                                            child: Text('确定',
+                                            child: const Text('确定',
                                                 style: TextStyle(
                                                   fontFamily: "NotoSerifSC",
                                                 )),
@@ -243,13 +268,14 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
                                   });
                                 },
                                 minWidth: double.infinity,
-                                shape: RoundedRectangleBorder(
+                                shape: const RoundedRectangleBorder(
                                     side: BorderSide(
                                         color: Colors.white, width: 1),
                                     borderRadius:
                                         BorderRadius.all(Radius.circular(200))),
-                                padding: EdgeInsets.symmetric(vertical: 18),
-                                child: Text(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 18),
+                                child: const Text(
                                   "提交",
                                   style: TextStyle(
                                     fontSize: 18,
@@ -259,7 +285,7 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
                                   ),
                                 ),
                               ),
-                              SizedBox(
+                              const SizedBox(
                                 height: 30,
                               ),
                               Row(
@@ -299,6 +325,33 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
       ]),
       VideoPage(videoPage: videoPage, controller: _controller),
       isShow1 ? FirstInvitePage() : Container(),
+      showLoding
+          ? Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: const Color(0xfffffcfc),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/images/010-flamenco-dancers-sexy-couple-silhouettes.png",
+                      width: 100,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      loadingMsg,
+                      style:
+                          _textStyle.copyWith(fontSize: 22, color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+            )
+          : Container(),
       Offstage(
         offstage: true,
         child: Column(
@@ -392,22 +445,23 @@ class _WeddingInvitePageState extends State<WeddingInvitePage> {
                     "assets/images/下花边.png",
                     color: Colors.white,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 15,
                   ),
                   Expanded(
                       child: Container(
-                    child: Image.asset("assets/images/AM2I7660.jpg"),
                     decoration: BoxDecoration(
                         border: Border.all(
                       color: Colors.white,
                     )),
+                    child: Image.asset("assets/images/AM2I7660.jpg"),
                   )),
                   Expanded(
                       child: GestureDetector(
                     onTap: () {
                       setState(() {
                         this.isShow = false;
+                        bgmManger.player.play();
                         _controller.play();
                       });
                     },
@@ -495,6 +549,9 @@ class HotelPageOne extends StatelessWidget {
                   )
                 ],
               ),
+              SizedBox(
+                height: 5,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -513,6 +570,9 @@ class HotelPageOne extends StatelessWidget {
                     ),
                   )
                 ],
+              ),
+              SizedBox(
+                height: 5,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -533,6 +593,9 @@ class HotelPageOne extends StatelessWidget {
                   )
                 ],
               ),
+              SizedBox(
+                height: 5,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -546,13 +609,13 @@ class HotelPageOne extends StatelessWidget {
                   ),
                   Expanded(
                     child: Text(
-                      "地铁11号线坐至顺帆北路2号口 或 公交至同丰路黄浦江路站、军泽园站",
+                      "地铁11号线坐至顺帆北路2号口\n公交至同丰路黄浦江路站、军泽园站",
                       style: _textStyle,
                     ),
                   )
                 ],
               ),
-              const SizedBox(height: 60),
+              const SizedBox(height: 30),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -561,7 +624,7 @@ class HotelPageOne extends StatelessWidget {
                     quarterTurns: 2,
                     child: Image.asset(
                       "assets/images/ezgif-3-88a030c11b.gif",
-                      height: 30,
+                      height: 20,
                     ),
                   ),
                   const Text(
@@ -574,12 +637,22 @@ class HotelPageOne extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white, width: 5)),
-                child: Image.asset(
-                  "assets/images/mapImage1.png",
-                  fit: BoxFit.cover,
+              GestureDetector(
+                onTap: () async {
+                  const url = 'https://surl.amap.com/da7Zcoz146B7';
+                  if (await canLaunchUrl(Uri.parse(url))) {
+                    await launchUrl(Uri.parse(url));
+                  } else
+                    // can't launch url, there is some error
+                    throw "Could not launch $url";
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 5)),
+                  child: Image.asset(
+                    "assets/images/mapImage1.png",
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               const SizedBox(height: 30),
